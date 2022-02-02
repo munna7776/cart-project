dee2e26e209bb43ba4199cd50b25486cd58ab42a
Munna Kumar Barnwal <barnwalmunnakumar@gmail.com>
2022-02-02 16:54:16 +0530

    Increase , decrease , delete functionality added

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,39 +4,10 @@ import Plus from './img/plus.png';
 import Minus from './img/minus-sign.png';
 import Bin from './img/bin.png';
 class CartItem extends Component {
-    constructor(){
-        super();
-        this.state={
-            title:'Xiomi',
-            price:15999,
-            qty : 1,
-            img : '',
-        }
-    }
-
-
-    increaseQuantity = ()=>{
-        this.setState((prevState)=>{
-            return {
-                qty : prevState.qty +1
-            }
-        })
-    }
-
-    decreaseQuantity = ()=>{
-        const {qty} = this.state;
-        if(qty === 0){
-            return;
-        }
-        this.setState((prevState)=>{
-            return {
-                qty : prevState.qty - 1
-            }
-        })
-    }
     
   render() {
-    const {title,price,qty} = this.state;
+    const {id,title,price,qty} = this.props.product;
+    const {product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct} = this.props
     return (
         <div className="cart-item">
             <div className="left-block">
@@ -52,19 +23,19 @@ class CartItem extends Component {
                         alt="increase" 
                         className="action-icons" 
                         src={Plus} 
-                        onClick={this.increaseQuantity}
+                        onClick={()=> onIncreaseQuantity(product)}
                     />
                     <img 
                         alt="decrease" 
                         className="action-icons" 
                         src={Minus}
-                        onClick = {this.decreaseQuantity}
-                        disabled = {true}
+                        onClick = {()=> onDecreaseQuantity(product)}
                     />
                     <img 
                         alt="delete" 
                         className="action-icons" 
                         src={Bin} 
+                        onClick={() => onDeleteProduct(product.id)}
                     />
                 </div>
             </div>
